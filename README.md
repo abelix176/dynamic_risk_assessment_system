58a5b5910201347b4788891486028cf9f25dceeb
Felix Abrecht <felix.abrecht@gmail.com>
2023-06-16 09:42:03 +0200

    create ingested dataset

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # dynamic_risk_assessment_system
 This is a "fork" of the project "build a dynamic risk assessment system" in the Udactiy nanodegree ML DevOps. 
+
+# Environment setup
+I chose to create my own environment with conda and install the requirements.txt with pip instead of using the workspace. This way, I can work in vscode and have full control over the dev env:
+
+```
+conda create -n risk_assessment python=3.7.6
+pip install -r requirements.txt
+```
--- ingestion.py
+++ ingestion.py
@@ -14,11 +14,33 @@ with open('config.json','r') as f:
 input_folder_path = config['input_folder_path']
 output_folder_path = config['output_folder_path']
 
-
+if not os.path.exists(output_folder_path):
+    os.makedirs(output_folder_path)
 
 #############Function for data ingestion
 def merge_multiple_dataframe():
-    #check for datasets, compile them together, and write to an output file
+    # Initialize an empty list to store the data from each CSV file
+    dataframes = []
+
+    # Iterate over each file in the input folder
+    for filename in os.listdir(input_folder_path):
+        if filename.endswith('.csv'):
+            file_path = os.path.join(input_folder_path, filename)
+            df = pd.read_csv(file_path)
+            dataframes.append(df)
+
+    # Combine all dataframes into a single dataframe
+    combined_df = pd.concat(dataframes)
+
+    # Remove duplicate rows
+    combined_df = combined_df.drop_duplicates().reset_index(drop=True)
+
+    # Save the combined dataframe as a CSV file
+    output_file_path = os.path.join(output_folder_path, 'finaldata.csv')
+    combined_df.to_csv(output_file_path, index=False)
+
+    # Print a confirmation message
+    print(f"The combined dataframe has been saved to: {output_file_path}")
 
 
 
