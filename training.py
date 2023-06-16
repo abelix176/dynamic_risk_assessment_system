13ab1907bf005f9e3094f27910200afa430af40f
Felix Abrecht <felix.abrecht@gmail.com>
2023-06-16 10:45:11 +0200

    training model

--- training.py
+++ training.py
@@ -7,26 +7,41 @@ from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import json
+import pandas as pd
 
 ###################Load config.json and get path variables
 with open('config.json','r') as f:
     config = json.load(f) 
 
 dataset_csv_path = os.path.join(config['output_folder_path']) 
-model_path = os.path.join(config['output_model_path']) 
+model_path = os.path.join(config['output_model_path'])
+if not os.path.exists(model_path):
+    os.makedirs(model_path)
 
 
 #################Function for training the model
 def train_model():
     
     #use this logistic regression for training
-    LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
+    model = LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
                     intercept_scaling=1, l1_ratio=None, max_iter=100,
-                    multi_class='warn', n_jobs=None, penalty='l2',
+                    multi_class='auto', n_jobs=None, penalty='l2',
                     random_state=0, solver='liblinear', tol=0.0001, verbose=0,
                     warm_start=False)
     
     #fit the logistic regression to your data
-    
-    #write the trained model to your workspace in a file called trainedmodel.pkl
+    # Read the dataset from finaldata.csv
+    dataset = pd.read_csv(os.path.join(dataset_csv_path, 'finaldata.csv'))
+    X = dataset[['lastmonth_activity', 'lastyear_activity', 'number_of_employees']]
+    y = dataset['exited']
 
+    model.fit(X, y)
+    print(f"Fitting model.")
+
+    # write the trained model to your workspace in a file called trainedmodel.pkl
+    with open(os.path.join(model_path, 'trainedmodel.pkl'), 'wb') as f:
+        pickle.dump(model, f)
+    print(f"The trained model has been saved to: {model_path}")
+
+if __name__ == '__main__':
+    train_model()
