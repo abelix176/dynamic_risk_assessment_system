6e9d327252bdace296141722ec6dc0a9357219f8
Felix Abrecht <felix.abrecht@gmail.com>
2023-06-16 10:23:53 +0200

    save ingested files record

--- ingestion.py
+++ ingestion.py
@@ -21,6 +21,7 @@ if not os.path.exists(output_folder_path):
 def merge_multiple_dataframe():
     # Initialize an empty list to store the data from each CSV file
     dataframes = []
+    file_records = []
 
     # Iterate over each file in the input folder
     for filename in os.listdir(input_folder_path):
@@ -28,6 +29,11 @@ def merge_multiple_dataframe():
             file_path = os.path.join(input_folder_path, filename)
             df = pd.read_csv(file_path)
             dataframes.append(df)
+
+            # Prepare ingest record
+            dateTimeObj=datetime.now()
+            thetimenow=str(dateTimeObj.year)+ '/'+str(dateTimeObj.month)+ '/'+str(dateTimeObj.day)
+            file_records.append([input_folder_path,filename,len(df.index),thetimenow])
 
     # Combine all dataframes into a single dataframe
     combined_df = pd.concat(dataframes)
@@ -39,8 +45,16 @@ def merge_multiple_dataframe():
     output_file_path = os.path.join(output_folder_path, 'finaldata.csv')
     combined_df.to_csv(output_file_path, index=False)
 
+    # Save ingest records to file
+    ingested_files_path = os.path.join(output_folder_path, 'ingestedfiles.txt')
+    with open(ingested_files_path, 'w') as ingested_files_file:
+        for sublist in file_records:
+            line = ','.join(str(element) for element in sublist)
+            ingested_files_file.write(line + '\n')
+
     # Print a confirmation message
     print(f"The combined dataframe has been saved to: {output_file_path}")
+    print(f"The list of ingested filenames has been saved to: {ingested_files_path}")
 
 
 
