832ebfec802adff2f3540d0126f58a2baf3292a2
Felix Abrecht <felix.abrecht@gmail.com>
2023-06-16 11:09:26 +0200

    deployment

--- deployment.py
+++ deployment.py
@@ -7,6 +7,7 @@ from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import json
+import shutil
 
 
 
@@ -15,12 +16,39 @@ with open('config.json','r') as f:
     config = json.load(f) 
 
 dataset_csv_path = os.path.join(config['output_folder_path']) 
-prod_deployment_path = os.path.join(config['prod_deployment_path']) 
+prod_deployment_path = os.path.join(config['prod_deployment_path'])
+model_path = os.path.join(config['output_model_path'])
+
+if not os.path.exists(prod_deployment_path):
+    os.makedirs(prod_deployment_path)
 
 
 ####################function for deployment
-def store_model_into_pickle(model):
+def store_model_into_pickle():
     #copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
+    
+    # Copy trained model
+    model_src = os.path.join(model_path, 'trainedmodel.pkl')
+    model_dest = os.path.join(prod_deployment_path, 'trainedmodel.pkl')
+    shutil.copy(model_src, model_dest)
+    print(f"Copied: {model_src} to: {model_dest}")
+
+    # Copy trained model
+    score_src = os.path.join(model_path, 'latestscore.txt')
+    score_dest = os.path.join(prod_deployment_path, 'latestscore.txt')
+    shutil.copy(score_src, score_dest)
+    print(f"Copied: {score_src} to: {score_dest}")
+
+    # Copy trained model
+    ingest_src = os.path.join(dataset_csv_path, 'ingestedfiles.txt')
+    ingest_dest = os.path.join(prod_deployment_path, 'ingestedfiles.txt')
+    shutil.copy(ingest_src, ingest_dest)
+    print(f"Copied: {ingest_src} to: {ingest_dest}")
+
+if __name__ == '__main__':
+    store_model_into_pickle()
+
+
         
         
         
